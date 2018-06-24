f2403ff4c8d9c32b7d837fe0de3bfec30083f86a
Ravi Prakash <prakashravi@maaii.com>
2018-06-24 08:23:33 +0800

    Initial Commit

---
+++ Transference/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transference
+//
+//  Created by Ravi Prakash on 6/24/18.
+//  Copyright © 2018 xpd54. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
