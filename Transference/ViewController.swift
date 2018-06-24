9a27a39961108934e63e64e6d33eaaace7f30d10
Ravi Prakash <prakashravi@maaii.com>
2018-06-24 16:18:34 +0800

    Add user signup flow

--- Transference/ViewController.swift
+++ Transference/ViewController.swift
@@ -8,21 +8,45 @@
 
 import UIKit
 import FirebaseUI
-class ViewController: UIViewController, FUIAuthDelegate {
+class ViewController: UIViewController {
     let authUI = FUIAuth.defaultAuthUI()!
+    var user = Auth.auth().currentUser
     override func viewDidLoad() {
+        // Set Display name
+        self.title = Label.Title
 
         // setup signup methods for firebase
         authUI.delegate = self
         let providers: [FUIAuthProvider] = [FUIGoogleAuth()]
         self.authUI.providers = providers
-        let authViewController = authUI.authViewController()
-        self.present(authViewController, animated: true, completion: nil)
+
+        // check if current user is still login
+        if user != nil {
+            self.title = user?.displayName;
+        } else {
+            showSignInUI()
+        }
+
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    func showSignInUI() {
+        let authViewController = authUI.authViewController()
+        self.present(authViewController, animated: true, completion: nil)
     }
 }
 
+extension ViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        // set user name as title
+        self.title = authDataResult?.user.displayName
+    }
+}
