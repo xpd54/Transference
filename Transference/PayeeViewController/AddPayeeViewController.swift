fcef95f0399084d6d3d991bbbb8011c6bf7d6e7b
Ravi Prakash <raviprakash.xpd54@gmail.com>
2018-06-26 08:13:16 +0800

    Add Payee screen

---
+++ Transference/PayeeViewController/AddPayeeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddPayeeViewController.swift
+//  Transference
+//
+//  Created by Ravi Prakash on 6/26/18.
+//  Copyright © 2018 xpd54. All rights reserved.
+//
+
+import UIKit
+
+class AddPayeeViewController: UIViewController {
+
+    @IBOutlet var name: UITextField!
+    @IBOutlet var email: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
